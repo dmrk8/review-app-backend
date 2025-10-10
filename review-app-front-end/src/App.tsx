4d6eab2aa60055329a380ebacf05eb6ffd46e552
AnilDemirok <anildemirok8@gmail.com>
2025-10-10 16:01:27 +0300

    deploy ready(full of bugs, basic UI)

--- review-app-front-end/src/App.tsx
+++ review-app-front-end/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/context/AuthProvider';
-import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Login/Register';
 import Dashboard from './pages/Dashboard/Dashboard';
@@ -13,7 +12,6 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />{' '}
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
